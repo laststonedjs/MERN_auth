ebee4fdeb8654ebef2b76220f132191c08561ee2
Filip <filipsarovic428@gmail.com>
2023-07-20 02:20:25 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Server is ready!'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
